479ae1e324ba4e661e3aa2b39fdef34e9efa650f
Aref <Arefkeyhani.pr@gmail.com>
2024-09-01 00:28:01 +0330

    showing the questions

--- JS/game.js
+++ JS/game.js
@@ -1,10 +1,20 @@
+//Variables
 import formatData from "./helper.js";
 
 const url =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null;
+
 const loader = document.getElementById("loader");
+
 const container = document.getElementById("container");
+
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
+
+let questinIndex = 0;
+let correctAnswer = null;
+//functions
 
 const fetchData = async () => {
   const response = await fetch(url);
@@ -15,8 +25,19 @@ const fetchData = async () => {
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
 
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } = formattedData[questinIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+
+//Event listeners
 window.addEventListener("load", fetchData);

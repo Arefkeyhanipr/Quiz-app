fbb75cbac38cc7e003ea8c2fda66726fdae475e5
Aref <Arefkeyhani.pr@gmail.com>
2024-09-06 07:14:53 +0330

    saving the result of the answers to the questions

---
+++ JS/result.js
@@ -0,0 +1,28 @@
+const score = JSON.parse(localStorage.getItem("score"));
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const scoreElement = document.querySelector("p");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+scoreElement.innerText = score;
+
+//function
+
+const saveHandler = () => {
+  if (!input.value) {
+    alert("invalid username");
+  } else {
+    const finalScore = { name: input.value, score: score };
+    highScores.push(finalScore);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(10);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.removeItem("scores");
+    window.location.assign("/");
+  }
+};
+
+//add event listener
+
+button.addEventListener("click", saveHandler);
